3cd356bd4eba83f3dce6b36c9d70a958f51bfe2e
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2023-04-12 05:24:02 +0100

    validation1

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -61,14 +61,7 @@ class UserType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -86,7 +79,14 @@ class UserType extends AbstractType
                     ]),
                 ],
             ])
-
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
 
         ;
     }
